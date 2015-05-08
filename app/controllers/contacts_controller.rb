79872e01e5e9eaf95dcfd9638a58240a5df7e1a3
J. Adam Kraft <jadamkraft@gmail.com>
2015-05-08 14:45:57 -0500

    added flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,20 @@ class ContactsController < ApplicationController
 	end
 
 	def create
+		@contact = Contact.new(contact_params)
+
+		if @contact.save
+			flash[:success] = 'Thank you for your contribution. We are building community one person at a time.'
+			redirect_to new_contact_path
+		else
+			flash[:error] = 'Your message was not received. Please try again.'
+			redirect_to new_contact_path
+		end
 	end
+
+	private
+	    def contact_params
+	        params.require(:contact).permit(:name, :email, :comments)
+	    end
+
 end
